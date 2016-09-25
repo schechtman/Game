7715302e9c1c4c7e2eeafe5a4bb5e01def597b8e
schechtman <sch.sholom@gmail.com>
2016-09-25 06:23:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+
+var game =[];
+for (var i=0;i<16;i++){
+		game.push(0)
+	}
+function random(){
+	var zeros=[];
+	for (var i=0;i<16;i++){
+		if (game[i]==0)
+			zeros.push(i)
+	}
+	var n=zeros.length;
+
+	var index=Math.floor((Math.random()*n));
+	var two_or_four=Math.floor((Math.random*10)+1);
+	if (Math.floor((Math.random*10)+1)==10)
+		game[zeros[index]]=4;
+	else
+		game[zeros[index]]=2;
+	
+	
+}
+function draw_grille(){
+for (var i=0;i<4;i++){
+		for (var j=0;j<4;j++){
+			var number=game[4*i+j]
+			if (number==0){
+				document.getElementById('tile'+i.toString()+j.toString()).innerHTML="";}
+			else{
+			document.getElementById('tile'+i.toString()+j.toString()).innerHTML=game[4*i+j];};
+		}
+}
+}
+
+function restart(){
+
+
+var ini1=Math.floor((Math.random() * 16) ); 
+var ini2=Math.floor((Math.random() * 16) ); 
+if (ini2===ini1){
+	ini1++;
+	ini1=ini1%16;
+	}
+for (var i=0;i<16;i++){
+		game[i]=0
+}
+game[ini1]=2*Math.floor((Math.random()*2)+1);
+game[ini2]=2*Math.floor((Math.random()*2)+1);
+draw_grille();
+
+}
+restart();
+
+		var check;
+		var game2;
+	    function move(e){
+		console.log('fdhqjkhqj');
+    	var reponse=modify_grid(game,e.keyCode);
+		game2=reponse[0];
+		check=reponse[1];
+
+
+		if (check){
+		console.log('par exemple');
+		game=game2;
+		
+		random();
+    	draw_grille();
+    }
+    else{ var fin=true;
+    	  var i=0;
+    	  console.log('et non');
+    	while (fin&&i<16){
+    		if (game[i]==0){
+    			fin=false;
+    		}
+    		else
+    		    i++;
+    	}
+    	if (fin){
+    		alert("Game Over");
+    		restart();
+    	}
+    	}
+	    }
+document.addEventListener("keydown",move,false);
+
+
+
+
---
+++ modify.js
@@ -0,0 +1,251 @@
+function modify_grid(grid,direction){
+	console.log('ouidfjk');
+	var change=false;
+	var row=[[],[],[],[]];
+	var grid_r=[];
+	if (!(direction===37||direction==38||direction==39||direction===40)){
+		return [grid,change];}
+	else if (direction==37||direction==39){
+		for (var i=0;i<4;i++){
+			for (var j=0;j<4;j++){
+
+			row[i].push(game[4*i+j]);
+
+		}
+
+		}
+		for (var i=0;i<4;i++){
+
+
+		
+		if (direction==37){
+
+			for (var i=0;i<4;i++){
+
+				var row1=row[i];
+				var row_inter=[];
+				var first=false;
+				var begin=-1;
+
+				for (var j=0;j<4;j++){
+						if (row1[j]!=0){
+							
+							if (!first) {
+
+							begin=row1[j];
+							first=true;}
+							else if (begin==row1[j]){
+								row_inter.push(2*begin);
+								
+								
+								first=false;
+
+								}
+							else{
+								row_inter.push(begin);
+
+								begin=row1[j];
+								
+						}
+						
+						
+				
+						}
+				}
+			if (first)
+				row_inter.push(begin);
+				
+
+			for (var j=0;j<4;j++){
+					if (j<row_inter.length)
+						{row[i][j]=row_inter[j];}
+					else
+						row[i][j]=0;
+					
+				}
+
+			}
+			for (var i=0;i<4;i++){
+				for (var j=0;j<4;j++){
+					grid_r.push(row[i][j]);
+					if (row[i][j]!=grid[4*i+j])
+						{change=true;}
+				}
+			}
+
+			}
+		else if (direction==39){
+
+			for (var i=0;i<4;i++){
+
+				var row1=row[i];
+				var row_inter=[];
+				var first=false;
+				var begin=-1;
+
+				for (var j=0;j<4;j++){
+						if (row1[3-j]!=0){
+							
+							if (!first) {
+							begin=row1[3-j];
+							first=true;}
+							else if (begin==row1[3-j]){
+								row_inter.push(2*begin);
+								first=false;
+								}
+							else{
+								row_inter.push(begin);
+								
+								begin=row1[3-j];
+								
+						}
+						
+						
+				
+						}
+				}
+			if (first)
+				row_inter.push(begin);
+
+
+			for (var j=0;j<4;j++){
+					if (j<row_inter.length)
+						{row[i][3-j]=row_inter[j];}
+					else
+						row[i][3-j]=0;
+					
+				}
+
+			}
+			for (var i=0;i<4;i++){
+				for (var j=0;j<4;j++){
+					grid_r.push(row[i][j])
+					if (row[i][j]!=grid[4*i+j])
+						{change=true;}
+				}
+			}
+
+			}
+		
+		
+	}
+	}
+		else if (direction==38||direction==40){
+		for (var i=0;i<4;i++){
+			for (var j=0;j<4;j++){
+
+			row[j].push(game[4*i+j]);
+
+		}
+
+		}
+
+
+		
+		if (direction==38){
+
+			for (var i=0;i<4;i++){
+
+				var row1=row[i];
+				var row_inter=[];
+				var first=false;
+				var begin=-1;
+
+				for (var j=0;j<4;j++){
+						if (row1[j]!=0){
+							
+							if (!first) {
+							begin=row1[j];
+							first=true;}
+							else if (begin==row1[j]){
+								row_inter.push(2*begin);
+								first=false;
+								}
+							else{
+								row_inter.push(begin);
+								
+								begin=row1[j];
+								
+						}
+						
+						
+				
+						}
+				}
+			if (first)
+				row_inter.push(begin);
+				
+
+			for (var j=0;j<4;j++){
+					if (j<row_inter.length)
+						{row[i][j]=row_inter[j];}
+					else
+						row[i][j]=0;
+					
+				}
+
+			}
+			for (var i=0;i<4;i++){
+				for (var j=0;j<4;j++){
+					grid_r.push(row[j][i])
+					if (row[j][i]!=grid[4*i+j])
+						{change=true;}
+				}
+			}
+
+			}
+		else if (direction==40){
+
+			for (var i=0;i<4;i++){
+
+				var row1=row[i];
+				var row_inter=[];
+				var first=false;
+				var begin=-1;
+
+				for (var j=0;j<4;j++){
+						if (row1[3-j]!=0){
+							
+							if (!first) {
+							begin=row1[3-j];
+							first=true;}
+							else if (begin==row1[3-j]){
+								row_inter.push(2*begin);
+								first=false;
+								}
+							else{
+								row_inter.push(begin);
+								begin=row1[3-j];
+								
+						}
+						
+						
+				
+						}
+				}
+			if (first)
+				row_inter.push(begin);
+
+
+			for (var j=0;j<4;j++){
+					if (j<row_inter.length)
+						{row[i][3-j]=row_inter[j];}
+					else
+						row[i][3-j]=0;
+					
+				}
+
+			}
+			for (var i=0;i<4;i++){
+				for (var j=0;j<4;j++){
+					grid_r.push(row[j][i])
+				if (row[j][i]!=grid[4*i+j])
+						{change=true;}
+				}
+			}
+
+			}
+	}
+
+	return [grid_r,change]
+}
